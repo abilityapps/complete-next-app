7e7fdae32054f8f959123c920b9ea24a11e19da7
Shouryan Nikam <snnikam@umich.edu>
2023-12-25 10:23:45 -0500

    test husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { serverClient } from "@/app/_trpc/serverClient";
+import {serverClient} from "@/app/_trpc/serverClient";
 import PeopleList from "@/components/TodoList";
 
 export default async function Home() {
 	const people = await serverClient.getAllPeople();
 	return (
 		<div>
+			Done
 			<PeopleList initialPeople={people} />
 		</div>
 	);
