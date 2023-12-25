eef16092b82965d0ae6694956c47560d2762644d
Shouryan Nikam <snnikam@umich.edu>
2023-12-25 10:37:15 -0500

    formatting

--- src/app/_trpc/TRPCProvider.tsx
+++ src/app/_trpc/TRPCProvider.tsx
@@ -1,10 +1,10 @@
 "use client";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
-import {httpBatchLink} from "@trpc/client";
-import React, {useState} from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { httpBatchLink } from "@trpc/client";
+import React, { useState } from "react";
 
-import {trpc} from "./client";
+import { trpc } from "./client";
 
 export default function TRPCProvider({
 	children,
