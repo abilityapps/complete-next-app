adb18f577493cd0769b1c575795c1837d67161d3
Shouryan Nikam <dev.shouryan@gmail.com>
2024-09-22 21:26:51 -0400

    enforce env variables

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,11 @@ import { z } from "zod";
 
 export const env = createEnv({
 	server: {
+		AUTH_SECRET: z.string().min(1),
+		AUTH_GOOGLE_ID: z.string().min(1),
+		AUTH_GOOGLE_SECRET: z.string().min(1),
+		AUTH_GITHUB_ID: z.string().min(1),
+		AUTH_GITHUB_SECRET: z.string().min(1),
 		// DATABASE_URL: z.string().url(),
 		// OPEN_AI_API_KEY: z.string().min(1),
 	},
